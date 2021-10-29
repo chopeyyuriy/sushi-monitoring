2cd7a1501a9c180b53b443e8b60c87d9678fd044
ajmaksss <maksym.bychkiv@gmail.com>
2021-10-29 10:31:50 +0300

    added pusher

--- src/views/dashboard/monitoring.jsx
+++ src/views/dashboard/monitoring.jsx
@@ -1,46 +1,103 @@
-import React, { Component, useEffect,useState } from 'react';
-import { Row, Col } from 'reactstrap';
-import FullCard from '../table/bootstrap/example/basicEx';
+import React, {useEffect, useState } from 'react';
+import DataTable from "react-data-table-component";
 import Pusher from 'pusher-js';
 
 const Monitoring = () => {
-    const [restaurants, setRestaurants] = useState();
+    const [restaurants, setRestaurants] = useState([]);
+    let allRestaurants = [];
+    const columns = [
+      {
+        name: "Ресторан",
+        selector: "restaurant",
+        sortable: true
+      },
+      {
+        name: "без доставки",
+        selector: "without_delivery",
+        sortable: true
+      },
+      {
+        name: "с доставкой",
+        selector: "with_delivery",
+        sortable: true,
+      },
+      {
+        name: "ваучеров  без доставки",
+        selector: "voucher_without_delivery",
+        sortable: true
+      },
+      {
+        name: "ваучеров  с доставкой",
+        selector: "voucher_with_delivery",
+        sortable: true
+      },
+      {
+        name: "Остаток по ваучерам",
+        selector: "voucher_max",
+        sortable: true,
+      }
+    ];
 
     useEffect(() => {
-        fetch('http://mysushi.rieltcrm.online/api/restaurants-settings', {
+      Pusher.logToConsole = true;
+      
+      const pusher = new Pusher('c5d9a52120e033bb2f56', {
+        app_id: "1289297",
+        key: "c5d9a52120e033bb2f56",
+        secret: "be74597ef3dbf8d0ec1c",
+        cluster: "eu",
+      });
+  
+      const channel = pusher.subscribe('setting-update');
+      channel.bind('setting-update', (data) => {
+        allRestaurants= data;
+        allRestaurants.response.restaurantsSettings.map((e,i) => {
+          setRestaurants(prevState => [...prevState,
+                  {
+                      id: i ,
+                      restaurant: e.name,
+                      without_delivery: e.settings[0].queue ,
+                      with_delivery:  e.settings[0].queue_delivery,
+                      voucher_without_delivery:  e.settings[0].queue_voucher,
+                      voucher_with_delivery:  e.settings[0].queue_voucher_delivery,
+                      voucher_max:  e.settings[0].max_voucher ,
+                  }
+              ])
+          })
+
+      });
+
+      fetch('http://mysushi.rieltcrm.online/api/restaurants-settings', {
             method: "GET"
         }).then(resp => resp.json())
-        .then(data => setRestaurants(data.response.restaurantsSettings))
-        .then(console.log(restaurants))
+          .then(data => {
+              data.response.restaurantsSettings.map((e,i) => {
+                  setRestaurants(prevState => [...prevState,
+                      {
+                          id: e.id ,
+                          restaurant: e.name,
+                          without_delivery: e.settings[0].queue ,
+                          with_delivery:  e.settings[0].queue_delivery,
+                          voucher_without_delivery:  e.settings[0].queue_voucher,
+                          voucher_with_delivery:  e.settings[0].queue_voucher_delivery,
+                          voucher_max:  e.settings[0].max_voucher ,
+                      }
+                  ])
+              })
+          })
     }, [])
 
+
     return (
-        <FullCard title='Очередь'>
-          <thead>
-            <tr>
-              <th>Ресторан</th>
-              <th>без доставки</th>
-              <th>с доставкой</th>
-              <th>ваучеров  без доставки</th>
-              <th>ваучеров  с доставкой</th>
-              <th>Остаток по ваучерам</th>
-            </tr>
-          </thead>
-          <tbody>
-              {restaurants &&
-                    restaurants.map((e,i) => (
-                        <tr key={i}>
-                            <td>{e.restaurants[0] ? `${++i}. ${e.restaurants[0].name}` : `${++i}.`}</td>
-                            <td>{e.queue}</td>
-                            <td>{e.queue_delivery}</td>
-                            <td>{e.queue_voucher}</td>
-                            <td>{e.queue_voucher_delivery}</td>
-                            <td>{e.max_voucher}</td>
-                        </tr>
-                    ))
-              }
-          </tbody>
-        </FullCard>
+        <DataTable
+          columns={columns}
+          data={restaurants}
+          noHeader
+          defaultSortField="id"
+          defaultSortAsc={false}
+          pagination
+          highlightOnHover
+        />
     )
   }
 export default Monitoring
